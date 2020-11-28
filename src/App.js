415c7a263478774813dabd26fdc16fa248fd3c91
Narayan Pandey <npandey722@gmail.com>
2020-11-28 17:33:02 +0530

    Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './App.css'
+import Header from './Header';
 function App() {
   return (
     <div className="app">
-      <h2>Amazon</h2>
+      <Header />
     </div>
   );
 }
