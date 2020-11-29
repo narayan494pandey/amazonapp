e65df405b0dc0155c76d158f47af384479371853
Narayan Pandey <npandey722@gmail.com>
2020-11-29 12:59:02 +0530

    before style

--- src/Product.js
+++ src/Product.js
@@ -3,7 +3,21 @@ import React from 'react'
 function Product() {
     return (
         <div className="product">
-            <h2>Product</h2>
+            <div className="product__info">
+                <p>The lean startup</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className="product__rating">
+                    <p>***</p>
+                </div>
+            </div>
+            <img
+                src="https://m.media-amazon.com/images/I/41Ag4WE7uyL._AC_UY218_.jpg"
+                alt="startup book"
+            />
+            <button>Add to Basket</button>
         </div>
     )
 }
