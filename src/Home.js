ab347a329f3bdf88df0d425e99bc972bb23e25ee
Narayan Pandey <npandey722@gmail.com>
2020-11-30 22:59:47 +0530

    props

--- src/Home.js
+++ src/Home.js
@@ -10,16 +10,46 @@ function Home() {
                 src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
             />
             <div className="home__row">
-                <Product />
-                <Product />
-                <Product />
+                <Product title='The lean startup' 
+                    price={29.99} 
+                    image='https://m.media-amazon.com/images/I/41Ag4WE7uyL._AC_UY218_.jpg' 
+                    rating={5}
+                />
+                <Product 
+                    title='The lean startup' 
+                    price={29.99} 
+                    image='https://m.media-amazon.com/images/I/41Ag4WE7uyL._AC_UY218_.jpg' 
+                    rating={5}
+                
+                
+                />
+                
             </div>
             <div className="home__row">
-                <Product />
-                <Product />
+                <Product title='The lean startup' 
+                    price={29.99} 
+                    image='https://m.media-amazon.com/images/I/41Ag4WE7uyL._AC_UY218_.jpg' 
+                    rating={5}
+                />
+                <Product title='The lean startup' 
+                    price={29.99} 
+                    image='https://m.media-amazon.com/images/I/41Ag4WE7uyL._AC_UY218_.jpg' 
+                    rating={5}
+                    
+                
+                
+                />
+                <Product   title='The lean startup' 
+                    price={29.99} 
+                    image='https://m.media-amazon.com/images/I/41Ag4WE7uyL._AC_UY218_.jpg' 
+                    rating={5}
+                    />
             </div>
             <div className="home__row">
-                <Product />
+                <Product title='The lean startup' 
+                    price={29.99} 
+                    image='https://m.media-amazon.com/images/I/41Ag4WE7uyL._AC_UY218_.jpg' 
+                    rating={5} />
             </div>
             </div>
         </div>
--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import './Product.css'
-function Product() {
+function Product({title, image,price,rating}) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>The lean startup</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                    <p>***</p>
+                    <p>***{rating}</p>
                 </div>
             </div>
             <img
-                src="https://m.media-amazon.com/images/I/41Ag4WE7uyL._AC_UY218_.jpg"
+                src={image}
                 alt="startup book"
             />
             <button>Add to Basket</button>
