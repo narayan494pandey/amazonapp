01d2f09eb061da7556438141bb1f1b648a641038
Narayan Pandey <npandey722@gmail.com>
2021-01-10 12:04:34 +0530

    contextApi

--- src/Home.js
+++ src/Home.js
@@ -12,12 +12,14 @@ function Home() {
             />
             <div className="home__row">
                 <Product title='Good Trimer for Save' 
+                    id="4591"
                     price={99.99} 
                     image='https://images-eu.ssl-images-amazon.com/images/G/31/img18/HPC/Personalcare/Shavers._CB440874771_.jpg' 
                     rating={4}
                 />
                 <Product 
-                    title='Nice looking smart watch' 
+                    title='Nice looking smart watch'
+                    id="4592" 
                     price={299.99} 
                     image='https://m.media-amazon.com/images/I/41aBcpoWWfL._AC_UL320_.jpg' 
                     rating={3}
@@ -28,11 +30,13 @@ function Home() {
             </div>
             <div className="home__row">
                 <Product title='The lean startup' 
+                    id="4593"
                     price={29.99} 
                     image='https://m.media-amazon.com/images/I/41Ag4WE7uyL._AC_UY218_.jpg' 
                     rating={4}
                 />
                 <Product title='Moile Holder' 
+                    id="4594"
                     price={9.99} 
                     image='https://images-eu.ssl-images-amazon.com/images/I/61sCL37xvlL._AC_UL200_SR200,200_.jpg' 
                     rating={3}
@@ -42,6 +46,7 @@ function Home() {
                 />
                 <Product   title='The Daily Nutrition' 
                     price={15.99} 
+                    id="4595"
                     image='https://images-eu.ssl-images-amazon.com/images/G/31/img18/HPC/Diet/Dailynutritionadults._CB442445732_.jpg' 
                     rating={4}
                     />
@@ -49,6 +54,7 @@ function Home() {
             <div className="home__row">
                 <Product title='Chocolates with amazing flavour ' 
                     price={20.99} 
+                    id="4596"
                     image='https://images-eu.ssl-images-amazon.com/images/G/31/img20/Grocery/GW/Chocolates_PC_CC_379x304_V2._SY304_CB404777310_.jpg' 
                     rating={5} />
             </div>
--- src/Product.js
+++ src/Product.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Product.css'
-function Product({title, image,price,rating}) {
+import {useStateValue} from "./StateProvider";
+function Product({id,title, image,price,rating}) {
+    const[{ basket }, dispatch] = useStateValue();
+    console.log('this is basket >>>', basket);
+    const addToBasket = () =>{
+        //dispatching the item into data layer
+        dispatch({
+            type:"Add_TO_BASKET",
+            item:{
+                id:id,
+                title:title,
+                image:image,
+                price:price,
+                rating:rating,
+            },
+        });
+    }
     return (
         <div className="product">
-            <div className="product__info">
+            <div className="product__info" >
                 <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
@@ -20,7 +36,7 @@ function Product({title, image,price,rating}) {
                 src={image}
                 alt="startup book"
             />
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
